e92f51f49dc78fbbe20905415fa7b12d5a739b52
imran2024bd <imranitbd34@gmail.com>
2024-03-25 10:36:07 +0600

    42-2 Create Fake Data For The Blogs And Load Data Done

--- src/component/Blogs/Blogs.jsx
+++ src/component/Blogs/Blogs.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Blogs = () => {
     const [blogs , setBlogs] = useState([]);
+    useEffect(() => {
+        fetch('blogs.json')
+        .then( res => res.json())
+        .then( data => setBlogs(data))
+        // .then( data => console.log(data))
+    },[])
     return (
         <div>
             
@@ -10,3 +16,17 @@ const Blogs = () => {
 };
 
 export default Blogs;
+
+
+
+/* Resource _ Cover_ img
+https://unsplash.com/s/photos/blog-cover
+
+https://www.freepik.com/free-photo/hand-drawn-paper-fathers-day_4086946.htm#from_view=detail_alsolike
+
+https://md-imran-hossain222.imgbb.com/
+
+Github_link
+https://github.com/ProgrammingHero1/react-knowledge-cafe
+
+*/
