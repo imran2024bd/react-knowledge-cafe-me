d433f68c66fd117c7624d3215548252834dbf59d
imran2024bd <imranitbd34@gmail.com>
2024-03-25 14:58:58 +0600

    42-4 Display Blog Data And Hashtags Done

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,10 +1,29 @@
 import PropTypes from 'prop-types';
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog }) => {
+    // console.log(blog);
+    const { title, cover, author_img, author, posted_date, reading_time, hashtags } = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between items-center'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'> {author} </h3>
+                        <p>{posted_date} </p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p className='gap-x-4' >
+                {
+                    hashtags.map((hash, idx) => <span  key={idx}><a href >#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
